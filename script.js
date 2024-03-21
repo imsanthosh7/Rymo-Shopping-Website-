31e46452a2a018cee9e3d355e21873b0f0103416
Santhosh <154437536+imsanthosh7@users.noreply.github.com>
2024-03-21 21:18:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+var mainimg = document.getElementById('main-img')
+var smallimg = document.getElementsByClassName('small-img')
+
+smallimg[0].onclick = function () {
+    mainimg.src = smallimg[0].src;
+
+}
+smallimg[1].onclick = function () {
+    mainimg.src = smallimg[1].src;
+
+}
+smallimg[2].onclick = function () {
+    mainimg.src = smallimg[2].src;
+
+}
+smallimg[3].onclick = function () {
+    mainimg.src = smallimg[3].src;
+
+}
+
+//  singleproduct click event 
+function abc() {
+    window.location.href = 'singleproduct.html'
+}
+
+
+
+
